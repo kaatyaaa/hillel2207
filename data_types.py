6ebfccb0311c2cbf1b5b9006408eaa37c31c3a55
kaatyaaa <vasilyevak22@gmail.com>
2022-07-30 01:20:14 +0300

    26072022 practical 1

---
+++ data_types.py
@@ -0,0 +1,48 @@
+method_str = 76
+method_int = 9
+result = method_str + method_int
+print (result)
+
+method_str = 473
+method_int = 126
+result = method_str - method_int
+print (result)
+
+method_str = 159
+method_int = 3
+result = method_str / method_int
+print (result)
+
+method_str = 72
+method_int = 90
+result = method_str * method_int
+print (result)
+
+
+result = 12 + 34 - 74 / 2 - 85 * 2
+print (result)
+
+new_method = 13
+print (type (new_method) , new_method)
+
+new_method: str = "my cool string"
+print (new_method , type (new_method))
+
+
+method_str = 21
+method_int = 43
+result = (method_str + method_int) / 8
+print (result)
+
+my_cool_str = "всем привет!"
+my_cool_str_2 = 'меня зовут Катя'
+my_cool_str_3 = '''это моя домашка'''
+my_cool_str_4 = """для hillel"""
+print (my_cool_str)
+print (my_cool_str_2)
+print (my_cool_str_3)
+print (my_cool_str_4)
+
+my_cool_str_5 = "пока"
+multiplication = my_cool_str_5 * 2
+print(multiplication)
