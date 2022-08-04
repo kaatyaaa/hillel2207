74acd831e42b031a2d0d039b895ba9293b0c9c4a
kaatyaaa <vasilyevak22@gmail.com>
2022-08-05 00:38:48 +0300

    04.08.22

---
+++ djdjd.py
@@ -0,0 +1,21 @@
+username: str = input("Please tell me your name")
+result = f"Hello , {username}, nice to meet you!"
+print(result)
+
+method_1 = float ("17.9")
+print (method_1, type (method_1))
+
+method_2 = float ("25.0")
+print (method_2, type (method_2))
+
+method_1 = int (73.1)
+print (method_1, type (method_1))
+
+exponentiation = method_2 ** 4
+print (exponentiation, type (exponentiation))
+
+square_root= method_2 ** 0.5
+print(square_root, type(square_root))
+
+division_remainder = method_2 % 2
+print(division_remainder, type(division_remainder))
---
+++ rows_and_numbers_advance.py
@@ -0,0 +1,16 @@
+str_method = "  kATya  "
+print(str_method)
+
+new_text = "  kATya  "
+print(new_text.strip())
+
+username: str = input("tell me your name")
+result = str.capitalize(username)
+result_2 = f"Hello, {username}, nice to meet you!"
+print(result_2)
+
+new_string = "kATya"
+print("lenght of string --> " , len(new_string))
+
+str_for_slice = "Katya"
+print(str_for_slice [: -6: -1])
